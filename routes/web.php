8165f18b443d4b46f9e1cdcfe2fd1e1518644904
root <root@DESKTOP-159IP4Q>
2023-09-28 16:32:11 +0800

    Full Page Components | Laravel Livewire 3 for Beginners EP15

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', App\Livewire\HomePage::class);
+
+Route::get('/users/{user}', App\Livewire\UserPage::class);
